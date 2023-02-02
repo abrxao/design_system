e1e340a1346eccf441b4c1464e073877a12c64f0
Abraão Albuquerque <abraao.hs2@gmail.com>
2023-02-02 17:20:26 -0300

    :wrench:chore: configuring storybook, next, prettier and eslint

---
+++ .storybook/preview.ts
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import * as NextImage from 'next/image';
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
